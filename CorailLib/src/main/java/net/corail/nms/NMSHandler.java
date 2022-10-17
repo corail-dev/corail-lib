5390d872b6f9dea81bfeaacbc973d38bc2a212dc
BuildTools <unconfigured@null.spigotmc.org>
2022-10-17 23:08:37 +0100

    17/10/2022 23:08

---
+++ CorailLib/src/main/java/net/corail/nms/NMSHandler.java
@@ -0,0 +1,7 @@
+package net.corail.nms;
+
+public interface NMSHandler {
+
+    void setBlockFast(int x, int y, int z, String world, int id, byte data);
+
+}
---
+++ NMS_1_7/src/main/java/net/corail/nms_1_7/NMSImpl_1_7.java
@@ -0,0 +1,10 @@
+package net.corail.nms_1_7;
+
+import net.corail.nms.NMSHandler;
+
+public class NMSImpl_1_7 implements NMSHandler {
+    @Override
+    public void setBlockFast(int x, int y, int z, String world, int id, byte data) {
+
+    }
+}
